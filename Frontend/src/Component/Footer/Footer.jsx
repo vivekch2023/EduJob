24a00cb10ac02c7b9ae4689c5396884f43956bdc
vivekch2023 <vivekchauhan99895@gmail.com>
2025-08-11 17:25:04 +0530

    Initial commit of MERN project

--- Frontend/src/Component/Footer/Footer.jsx
+++ Frontend/src/Component/Footer/Footer.jsx
@@ -1,224 +1,103 @@
 function Footer() {
+  const linkClass =
+    "hover:underline hover:text-white transition-colors duration-200";
+
   return (
     <footer className="bg-purple-900 text-white py-12 px-6 md:px-20 mt-20">
       <div className="grid grid-cols-1 md:grid-cols-4 gap-10 text-center md:text-left">
 
-        {/* word it company carrer site */}
+        {/* Global IT Career Sites */}
         <div>
-          <h3 className="text-lg font-semibold text-white mb-3">
-            Global IT Career Sites
-          </h3>
+          <h3 className="text-lg font-semibold mb-3">Global IT Career Sites</h3>
           <ul className="space-y-2 text-gray-300 text-sm">
-            <li>
-              <a
-                href="https://careers.microsoft.com"
-                className="hover:underline"
-              >
-                Microsoft
-              </a>
-            </li>
-            <li>
-              <a href="	https://www.amazon.jobs" className="hover:underline">
-                Amazon (AWS)
-              </a>
-            </li>
-            <li>
-              <a href="https://jobs.apple.com" className="hover:underline">
-                Apple
-              </a>
-            </li>
-            <li>
-              <a href="#	https://careers.google.com" className="hover:underline">
-                Google (Alphabet)
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.ibm.com/employment"
-                className="hover:underline"
-              >
-                IBM
-              </a>
-            </li>
-            <li>
-              <a
-                href="	https://www.oracle.com/corporate/careers/"
-                className="hover:underline"
-              >
-                Oracle
-              </a>
-            </li>
-            <li>
-              <a href="	https://www.metacareers.com" className="hover:underline">
-                Meta (Facebook)
-              </a>
-            </li>
-            <li>
-              <a
-                href="	https://www.accenture.com/in-en/careers"
-                className="hover:underline"
-              >
-                Accenture
-              </a>
-            </li>
-            <li>
-              <a
-                href="	https://www.capgemini.com/careers/"
-                className="hover:underline"
-              >
-                Capgemini
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://adobe.wd5.myworkdayjobs.com/en-US/external_experienced"
-                className="hover:underline"
-              >
-                Adobe
-              </a>
-            </li>
+            {[
+              { name: "Microsoft", url: "https://careers.microsoft.com" },
+              { name: "Amazon (AWS)", url: "https://www.amazon.jobs" },
+              { name: "Apple", url: "https://jobs.apple.com" },
+              { name: "Google (Alphabet)", url: "https://careers.google.com" },
+              { name: "IBM", url: "https://www.ibm.com/employment" },
+              { name: "Oracle", url: "https://www.oracle.com/corporate/careers/" },
+              { name: "Meta (Facebook)", url: "https://www.metacareers.com" },
+              { name: "Accenture", url: "https://www.accenture.com/in-en/careers" },
+              { name: "Capgemini", url: "https://www.capgemini.com/careers/" },
+              { name: "Adobe", url: "https://adobe.wd5.myworkdayjobs.com/en-US/external_experienced" },
+            ].map((item, idx) => (
+              <li key={idx}>
+                <a href={item.url} className={linkClass} target="_blank" rel="noopener noreferrer">
+                  {item.name}
+                </a>
+              </li>
+            ))}
           </ul>
         </div>
 
-        {/* Indian it carrer site*/}
+        {/* Indian IT Career Sites */}
         <div>
-          <h3 className="text-lg font-semibold text-white mb-3">
-            Indian IT Career Sites
-          </h3>
+          <h3 className="text-lg font-semibold mb-3">Indian IT Career Sites</h3>
           <ul className="space-y-2 text-gray-300 text-sm">
-            <li>
-              <a href="https://www.tcs.com/careers" className="hover:underline">
-                TCS (Tata Consultancy Services)
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.infosys.com/careers/"
-                className="hover:underline"
-              >
-                Infosys
-              </a>
-            </li>
-            <li>
-              <a href="	https://careers.wipro.com/" className="hover:underline">
-                Wipro
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.hcltech.com/careers"
-                className="hover:underline"
-              >
-                HCLTech (HCL Technologies)
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://careers.techmahindra.com/"
-                className="hover:underline"
-              >
-                Tech Mahindra
-              </a>
-            </li>
+            {[
+              { name: "TCS (Tata Consultancy Services)", url: "https://www.tcs.com/careers" },
+              { name: "Infosys", url: "https://www.infosys.com/careers/" },
+              { name: "Wipro", url: "https://careers.wipro.com/" },
+              { name: "HCLTech (HCL Technologies)", url: "https://www.hcltech.com/careers" },
+              { name: "Tech Mahindra", url: "https://careers.techmahindra.com/" },
+              { name: "Cognizant", url: "https://careers.cognizant.com/" },
+               { name: "LTIMindtree", url: "https://careers.ltimindtree.com" },
+                { name: "IBM India", url: "https://www.ibm.com/in-en/employment/" },
+            ].map((item, idx) => (
+              <li key={idx}>
+                <a href={item.url} className={linkClass} target="_blank" rel="noopener noreferrer">
+                  {item.name}
+                </a>
+              </li>
+            ))}
           </ul>
         </div>
 
-        {/* popular coding platform*/}
+        {/* Popular Coding Platforms */}
         <div>
-          <h3 className="text-lg font-semibold text-white mb-3">
-            Popular Coding Platforms
-          </h3>
+          <h3 className="text-lg font-semibold mb-3">Popular Coding Platforms</h3>
           <ul className="space-y-2 text-gray-300 text-sm">
-            <li>
-              <a href="https://www.tcs.com/careers" className="hover:underline">
-                LeetCode
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.infosys.com/careers/"
-                className="hover:underline"
-              >
-                HackerRank
-              </a>
-            </li>
-            <li>
-              <a href="	https://careers.wipro.com/" className="hover:underline">
-                Codeforces
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.hcltech.com/careers"
-                className="hover:underline"
-              >
-                GeeksforGeeks
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://careers.techmahindra.com/"
-                className="hover:underline"
-              >
-                CodeChef
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://careers.techmahindra.com/"
-                className="hover:underline"
-              >
-                InterviewBit
-              </a>
-            </li>
+            {[
+              { name: "LeetCode", url: "https://leetcode.com" },
+              { name: "HackerRank", url: "https://www.hackerrank.com" },
+              { name: "Codeforces", url: "https://codeforces.com" },
+              { name: "GeeksforGeeks", url: "https://www.geeksforgeeks.org" },
+              { name: "CodeChef", url: "https://www.codechef.com" },
+              { name: "InterviewBit", url: "https://www.interviewbit.com" },
+            ].map((item, idx) => (
+              <li key={idx}>
+                <a href={item.url} className={linkClass} target="_blank" rel="noopener noreferrer">
+                  {item.name}
+                </a>
+              </li>
+            ))}
           </ul>
         </div>
 
-        {/* Other links*/}
+        {/* Other Links */}
         <div>
-          <h3 className="text-lg font-semibold text-white mb-3">Other Links</h3>
+          <h3 className="text-lg font-semibold mb-3">Other Links</h3>
           <ul className="space-y-2 text-gray-300 text-sm">
-            <li>
-              <a href="https://www.tcs.com/careers" className="hover:underline">
-                LeetCode
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.infosys.com/careers/"
-                className="hover:underline"
-              >
-                HackerRank
-              </a>
-            </li>
-            <li>
-              <a href="	https://careers.wipro.com/" className="hover:underline">
-                Codeforces
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.hcltech.com/careers"
-                className="hover:underline"
-              >
-                GeeksforGeeks
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://careers.techmahindra.com/"
-                className="hover:underline"
-              >
-                CodeChef
-              </a>
-            </li>
+            {[
+             { name: "MDN Web Docs", url: "https://developer.mozilla.org" },
+             { name: "Stack Overflow", url: "https://stackoverflow.com" },
+              { name: "GitHub", url: "https://github.com" },
+              { name: "TechFetch ", url: "https://www.techfetch.com/" },
+            ].map((item, idx) => (
+              <li key={idx}>
+                <a href={item.url} className={linkClass}>
+                  {item.name}
+                </a>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
 
       {/* Bottom Section */}
       <div className="border-t border-purple-700 mt-10 pt-6 text-center text-sm text-gray-400">
-        <p>&copy; 2025 EduJobHub. All rights reserved.</p>
+        <p>&copy; {new Date().getFullYear()} EduJobHub. All rights reserved.</p>
         <p>Designed & Developed by Vivek Chauhan</p>
       </div>
     </footer>
