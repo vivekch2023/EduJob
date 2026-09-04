4872405b3c7abf6980c7191574c1ded7b6567cbf
vivekch2023 <166383849+vivekch2023@users.noreply.github.com>
2026-09-05 00:04:24 +0530

    Update HomePage.jsx

--- Frontend/src/Component/HomePage.jsx
+++ Frontend/src/Component/HomePage.jsx
@@ -111,7 +111,7 @@ export default function EduJobHubLandingPage() {
         <section className="py-20 px-6 md:px-20 bg-purple-50 text-center">
           <h2 className="text-4xl font-bold text-purple-700 mb-12">What Our Users Say</h2>
           <div className="grid md:grid-cols-3 gap-10">
-            {["Ritesh Kushwaha", "Himanshu Jaishwal", "Shanvi"].map((name, i) => (
+            {["Mr.Rahul", "Rohan Kumar", "Shilpi"].map((name, i) => (
               <motion.div key={name} className="bg-white p-6 rounded-lg shadow hover:shadow-lg" initial={{ opacity: 0, y: 30 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 + i * 0.2 }} viewport={{ once: true }}>
                 <p className="italic mb-4">
                   {[
@@ -180,7 +180,7 @@ export default function EduJobHubLandingPage() {
             />
 
             <div className="text-center md:text-left">
-              <h4 className="text-2xl md:text-3xl font-semibold text-gray-800">Vivek Chauhan</h4>
+              <h4 className="text-2xl md:text-3xl font-semibold text-gray-800">Zetavex Tech Solutions</h4>
               <p className="text-gray-600 mt-1">Full Stack Developer · Founder, EduJobHub</p>
 
               <p className="text-gray-700 mt-4 leading-relaxed max-w-xl">
@@ -189,11 +189,11 @@ export default function EduJobHubLandingPage() {
 
               <div className="mt-6 flex flex-wrap justify-center md:justify-start gap-8">
                 <a
-                  href="https://vivekchportfolio.netlify.app/"
+                  href="https://www.zetavextech.in/"
                   target="_blank"
                   className="text-sm px-4 py-2 border border-indigo-600 text-indigo-600 rounded-full hover:bg-indigo-50 transition"
                 >
-                  🌐 Portfolio
+                  🌐 Website
                 </a>
             
               </div>
